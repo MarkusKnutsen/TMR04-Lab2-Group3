743dd6335066e2d43f7a0b3579c9c279618966a2
MarkusKnutsen <markus.knutsen@hotmail.com>
2023-10-05 20:51:52 +0200

    Cleaned the code, separated functions, commented

--- data.py
+++ data.py
@@ -1,23 +1,13 @@
-from apread import APReader
-import scipy as sp
-import numpy as np
 from matplotlib import pyplot as plt
 import seaborn as sns
-import os
+
+# Importing functions from ./function.py in this diectory
+from functions import read_bin, gauss_filter, data_collecting_function, transverse_data, plot_signal
+
 sns.set_context("paper")
 sns.set_theme(style="darkgrid", palette="bright")
 
-# Data for the cylinder used in the experiment
-D1 = 0.029
-D2 = 0.025
-D3 = 0.032
-
-diameter = [0, D1, D1, D2, D2, D3, D3, 0, 0, 0]
-
-speed_levels = [0.4, 0.5, 0.6, 0.65, 0.7, 0.75, 0.80, 0.80, 0.80, 0.80, 0.80, 0.8, 0.9, 1, 1.1, 1.15, 1.2, 1.3, 1.4, 1.5]
-
-Re = [10000,12500,15000,16250,17500,18750,20000,20000,20000,20000,20000,21250,22500,25000,27500,28750,30000,32500,35000,37500]
-
+# \-- Channel Information --/
 # 0   Channel "Time  1 - default sample rate" (9495 Entries)
 # 1   Channel "1X" (9495 Entries)
 # 2   Channel "1Y" (9495 Entries)
@@ -27,117 +17,17 @@ Re = [10000,12500,15000,16250,17500,18750,20000,20000,20000,20000,20000,21250,22
 # 6   Channel "3Y" (9495 Entries)
 # 7   Channel "UL_WaterLevel" (9495 Entries)
 # 8   Channel "Position" (9495 Entries)
-# 9   Channel "Speed" (9495 Entries)
+# 9   Channel "velocity" (9495 Entries)
 
-# Function for reading in the data from the .BIN files in directory of choosing
+# \-- Data for the cylinders used in the experiment --/
 
-
-def read_bin(directory):
-    reader = []
-    for root, dirs, files in os.walk(directory):
-        for filename in sorted(files):
-            reader.append(APReader(os.path.join(root, filename)))
-    return reader
-
-# Function to filter the noise from the signal. The value of 50 is visually chosen, such that the noise is gone and the data is intact
-
-
-def gauss_filter(data, sigma=50):
-    gauss_filtered_signal = sp.ndimage.gaussian_filter1d(data, sigma)
-    return gauss_filtered_signal
-
-# Function to collect all the necessary variables for the plotting
-
-
-def data_collecting_function(run, sensor):
-    # Using the end time of the run to select the period. (Channel[0] is time, check the top of the code file)
-    sample_period = run.Channels[0].data.max()
-    # Using the time-channel to find out how many entries of data we have
-    samples = run.Channels[0].length
-    # Calculating the sample frequency of the run
-    sample_fq = samples / sample_period
-    # The x-axis will represent the number of sample points. to find the time, one could divide with the sample frequency
-    x_axis = np.linspace(0, samples, samples)
-    # The sensor variable, initialized on the top of the code file, selects the channel and gives the data. This code is for the Speed
-    data = run.Channels[sensor].data
-    # The name for use in the plot
-    name = run.Channels[sensor].Name
-    # The name of the file
-    filename = run.fileName
-    # Returns the filtered data
-    f1 = gauss_filter(data)
-    # Return the filtered speed data
-    speed = gauss_filter(run.Channels[9].data)
-
-    return (sample_fq, samples, sample_fq, x_axis, data, name, filename, f1, speed)
-
-# Function to find the amplitude data from the oscillatory data
-
-
-def peak_finder(data, h, prom, dist):
-    # First the data is selected, for both the natural values, but also with a flipped sign. This is to collect the negative amplitudes
-    # For this function, the positive data is in position 0, and the negative data in position 1
-    amplitudes = [data, -(data)]
-
-    # Finding the peak indices
-    peaks = [sp.signal.find_peaks(amplitudes[0], height=h, prominence=prom, distance=dist)[
-        0], sp.signal.find_peaks(amplitudes[1], height=h, prominence=prom, distance=dist)[0]]
-    
-    print('PEAKS:', peaks[0][:20])
-
-    # Initialazing the vector that will just contain the amplitudes, as to preform calculations on them
-    amp_val = [[], []]
-
-    # Initializing the vector containing the mean data of the amplitudes
-    mean_amp = [0, 0]
-
-    # Initializing the vector containing the maximum data of the amplitudes
-    max_amp = [0, 0]
-
-    # Used in the for loop to know what sign is being used
-    signs = [0, 1]
-
-    # Variable used for keeping track of the last peak that was investigated in the foor loop
-    last_peak = 0
-
-    # Iterating through the signs
-    for sign in signs:
-
-        # Iterating through the peak indices
-        for peak in peaks[sign]:
-
-            # The values between peaks are set to None, so that only the peaks show up in plots
-            amplitudes[sign][last_peak+1:peak-1].fill(None)
-
-            # Append the amplitude to the vector that will be used for calculations on the amplitudes. This cannot be done with elements that are 'None'
-            amp_val[sign].append(amplitudes[sign][peak])
-
-            # Updating the value for 'last_peak'
-            last_peak = peak
-
-        # amplitudes[sign][amplitudes[sign] == 0] = None
-        # Calcuating the mean and the maximum values from the amplitudes
-        mean_amp[sign] = np.array(amp_val[sign]).mean()
-        max_amp[sign] = np.array(amp_val[sign]).max()
-
-    plt.plot(amplitudes[0], 'ro')
-    plt.title("Positive")
-    plt.show()
-    plt.plot(-amplitudes[1], 'bo')
-    plt.title("Negative")
-    plt.show()
-
-        # Returns the vector of amplitude data
-    return [amplitudes[0], -amplitudes[1], mean_amp[0], -mean_amp[1], max_amp[0], -max_amp[1]]
-
-# --- Amplitude Data Indices---
-#   0 - Positive amplitudes vector. Used for plotting
-#   1 - Negative amplitudes vector. Used for plotting
-#   2 - Mean of the positive amplitudes
-#   3 - Mean of the negative amplitudes
-#   4 - Maximum of the positive amplitudes
-#   5 - Minimum of the negative amplitudes
-# -----------------------------
+# Diameter based on channel number
+diameter = [0, 0.029, 0.029, 0.025, 0.025, 0.032, 0.032, 0, 0, 0]
+# Velocity and Reynolds Number based on run number
+velocity_levels = [0.4, 0.5, 0.6, 0.65, 0.7, 0.75, 0.80, 0.80,
+                   0.80, 0.80, 0.80, 0.8, 0.9, 1, 1.1, 1.15, 1.2, 1.3, 1.4, 1.5]
+Re = [10000, 12500, 15000, 16250, 17500, 18750, 20000, 20000, 20000, 20000,
+      20000, 21250, 22500, 25000, 27500, 28750, 30000, 32500, 35000, 37500]
 
 # Reading in the data
 directory = './bin'
@@ -147,130 +37,78 @@ reader = read_bin(directory)
 sensors = [4]
 
 # Indices ues for the 2Y data. Data selected to remove the transient information
-Y2 = [[3400, 11200], [3200, 9200], [3200, 8400], [3100, 7600], [3700, 7700], [3300, 7100], [3800, 6700], [3200, 6700], [4000, 7300], [3700, 7000], [3500, 6800], [3500, 5600], [3400, 6400], [4000, 5900], [3340, 5730], [4160, 5550], [4440, 5500], [4830, 5400], [3770, 4850], [3750, 4920]]
+Y2 = [[3400, 11200], [3200, 9200], [3200, 8400], [3100, 7600], [3700, 7700], [3300, 7100], [3800, 6700], [3200, 6700], [4000, 7300], [3700, 7000], [
+    3500, 6800], [3500, 5600], [3400, 6400], [4000, 5900], [3340, 5730], [4160, 5550], [4440, 5500], [4830, 5400], [3770, 4850], [3750, 4920]]
 
 # List containing the index data
 indices = [0, 0, 0, 0, Y2, 0, 0, 0, 0, 0]
 
-# List for storing the largest forces
-max_forces = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+# List for storing the maximum value for the bending moment
+BendingMoment = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 # List for storing the strouhal number
 St = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-St2 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 # Variable to keep track of the runs. rc stands for run counter
 rc = 0
 
-# Will comment the code tomorrow.
-
+# Iterating through the different runs
 for run in reader:
+
+    # Iterating through the different sensors we want to investigate in that run
     for sensor in sensors:
 
-      s = indices[sensor]
+        # Selecting the steady state information indices to be used when processing the signal from the sensor
+        start = indices[sensor][rc][0]
+        end = indices[sensor][rc][1]
 
-      start = s[rc][0]
-      end = s[rc][1]
+        # Calculating variables from the signal
+        (sample_fq, samples, sample_fq, x_axis, data, name,
+         filename) = data_collecting_function(run, sensor)
 
-      (sample_fq, samples,  sample_fq, x_axis, data, name, filename, f1, speed) = data_collecting_function(run, sensor)
+        # Filter the data slightly, to remove some high-frequency noise in the peaks and valleys of the signal
+        signal = gauss_filter(data[start:end], 4)
 
-      signal = gauss_filter(data[start:end], 4)
+        # Calculating the Strouhal Number and maximum bending moment from the signal
+        strouhal, moment = transverse_data(
+            signal, diameter[sensor], velocity_levels[rc], sample_fq, end-start)
 
-      m = 200
+        # Adding the Stouhal data, and maximum bending moment to the vectors containing that data. To be used for plotting
+        St[rc] = strouhal
+        BendingMoment[rc] = moment
 
-      vec_pos = []
-      vec_neg = []
-      for elem in signal:
-          if elem < 0:
-              vec_pos.append(0)
-              vec_neg.append(elem)
-          if elem >= 0:
-              vec_pos.append(elem)
-              vec_neg.append(0)
-      mean_pos = np.round(np.array(vec_pos).mean(), 3)
-      mean_neg = -np.round(np.array(vec_neg).mean(), 3)
+        # If un-commented, the signal can be plotted
+        # plot_signal(signal, start, end, x_axis[start:end], name)
 
-      vec_neg = np.copysign(vec_neg, 1)
+        # Updating the run counter, and completing the iteration
+        rc += 1
 
-      added_signal = [value+m for value in signal]
-      manipulated_signal = [(value*(-1))+m for value in signal]
+# \-- Plotting --/
 
-      pos_peaks = sp.signal.find_peaks(added_signal, height=m*0.25, distance=30)[0]
-      neg_peaks = sp.signal.find_peaks(manipulated_signal, height=m*0.25, distance=30)[0]
+# Plotting the Strouhal Number against the Reynolds number. Both lines and points are plotted
+plt.plot(Re, St, color='hotpink', label='Strouhal Number')
+plt.plot(Re, St, 'o', color='crimson')
 
-      pos_amplitude = [None]
-      neg_amplitude = [None]
-
-      vec_neg = np.copysign(vec_neg, -1)
-
-      last_peak = 0
-      for peak in pos_peaks:
-        pos_amplitude.extend([None]*(peak-last_peak-1))
-        if vec_pos[peak] > 0: pos_amplitude.append(vec_pos[peak])
-        else: pos_amplitude.append(vec_neg[peak])
-        last_peak = peak
-      pos_amplitude.extend([None]*((end - start) - len(pos_amplitude)))
-
-      last_peak = 0
-      for peak in neg_peaks:
-        neg_amplitude.extend([None]*(peak-last_peak-1))
-        if vec_neg[peak] < 0: neg_amplitude.append(vec_neg[peak])
-        else: neg_amplitude.append(vec_pos[peak])
-        last_peak = peak
-      neg_amplitude.extend([None]*((end - start) - len(neg_amplitude)))
-      
-      # --- Calculating the strouhal number manually ---
-      peak_frequency = 1/(2*len(x_axis[start:end])/((len(pos_peaks) + len(neg_peaks))*sample_fq))
-
-      # FFT ----
-      from scipy.fft import fft, fftfreq
-      N = end-start
-
-      yf = fft(signal)
-      abs_yf = np.abs(yf)
-      xf = fftfreq(N, 1 / sample_fq)
-
-      max_index = np.where(abs_yf==max(abs_yf))[0][0]
-      
-      if xf[max_index] == 0:
-          abs_yf[max_index] = 0
-      
-      peak_frequency_2 = xf[np.where(abs_yf==max(abs_yf))[0][0]]
-
-      strouhal = (peak_frequency * diameter[sensor])/speed_levels[rc]
-      strouhal2 = (peak_frequency_2 * diameter[sensor])/speed_levels[rc]
-
-      St[rc] = strouhal
-      St2[rc] = strouhal2
-
-      max_forces[rc] = max(max(signal), abs(min(signal)))
-      # ----------------------------------------------------------------------
-
-      x = x_axis[start:end]
-
-      plt.plot(x, pos_amplitude, 'ro')
-      plt.plot(x, neg_amplitude, 'bo')
-      plt.plot(x, vec_pos, color='sandybrown')
-      plt.plot(x, vec_neg, color='lightskyblue')
-      plt.hlines(max(signal), start, end, color='lime')
-      plt.hlines(min(signal), start, end, color='lime')
-      plt.title(name + ' | Peaks = ' + str(len(pos_peaks) + len(neg_peaks)) + ' | Mean_pos = ' + str(mean_pos) + ', Mean_neg = ' + str(-mean_neg) 
-                + ' | Max_pos = ' + str(np.round(max(signal), 3)) + ', Max_neg = ' + str(np.round(min(signal), 3)) + ' | Strouhal Number = ' + str(np.round(strouhal, 3)))
-      plt.show()
-
-      rc += 1
-
-plt.plot(Re, St, color='hotpink', label='Manual method')
-plt.plot(Re, St2, color='slateblue', label='FFT method')
+# Labeling the units on the axis
 plt.xlabel('Re')
 plt.ylabel('St')
-plt.title('Strouhal Number, ' + name)
+
+# Title is selected
+plt.title('Strouhal Number, ' + name + ' ' + filename)
+
 plt.legend()
 plt.show()
 
-plt.plot(Re, max_forces, 'o', color='navy')
-plt.plot(Re, max_forces, color='steelblue')
+# Plotting the Bending moment against the Reynolds number. Both lines and points are plotted
+plt.plot(Re, BendingMoment, color='steelblue', label='Bending Moment')
+plt.plot(Re, BendingMoment, 'o', color='navy')
+
+# Labeling the units on the axis
 plt.ylabel('Nm')
 plt.xlabel('Re')
-plt.title('Bending Moment, ' + name)
+
+# Title is selected
+plt.title('Bending Moment, ' + name + ' ' + filename)
+
+plt.legend()
 plt.show()
---
+++ functions.py
@@ -0,0 +1,158 @@
+from apread import APReader
+from matplotlib import pyplot as plt
+import scipy as sp
+import numpy as np
+import os
+
+# Function for reading in the data from the .BIN files in directory of choosing
+
+
+def read_bin(directory):
+    reader = []
+    for root, dirs, files in os.walk(directory):
+        for filename in sorted(files):
+            reader.append(APReader(os.path.join(root, filename)))
+    return reader
+
+# Function to filter the noise from the signal. The value of 50 is default and can be changed
+
+
+def gauss_filter(data, sigma=50):
+    gauss_filtered_signal = sp.ndimage.gaussian_filter1d(data, sigma)
+    return gauss_filtered_signal
+
+# Function to collect all the necessary variables for the plotting
+
+
+def data_collecting_function(run, sensor):
+    # Using the end time of the run to select the period. (Channel[0] is time, check the top of the code file)
+    sample_period = run.Channels[0].data.max()
+    # Using the time-channel to find out how many entries of data we have
+    samples = run.Channels[0].length
+    # Calculating the sample frequency of the run
+    sample_fq = samples / sample_period
+    # The x-axis, for use in plotting. Represents sample points, but can be divided with the sample frequency to represent time
+    x_axis = np.linspace(0, samples, samples)
+    # The data from the selected channel
+    data = run.Channels[sensor].data
+    # The name of the sensor
+    name = run.Channels[sensor].Name
+    # The name of the file
+    filename = run.fileName
+
+    return (sample_fq, samples, sample_fq, x_axis, data, name, filename)
+
+# Function for calculating the strouhal number and the lift-induced peak bending moment.
+
+
+def transverse_data(signal, diameter, velocity, sample_fq, period):
+    # Fast Fourier Transform to get the vortex shedding frequency
+    yf = sp.fft.fft(signal)
+    xf = sp.fft.fftfreq(period, 1 / sample_fq)
+    abs_yf = np.abs(yf)
+
+    # Finding the most prominent shedding frequency
+    max_index = np.where(abs_yf == max(abs_yf))[0][0]
+
+    # Remove the frequency if it is 0
+    if xf[max_index] == 0:
+        abs_yf[max_index] = 0
+
+    # Selecting the most prominent non-zero shedding frequency
+    peak_frequency = xf[np.where(abs_yf == max(abs_yf))[0][0]]
+
+    # Calculating the Strouhal Number
+    St = (peak_frequency * diameter)/velocity
+
+    # The highest absolute value of the signal is selected as the max Bending Moment
+    BendingMoment = max(max(signal), abs(min(signal)))
+
+    return St, BendingMoment
+
+# Function for plotting the signal. The peaks are plotted, as well as the negative and positive bending moment
+
+
+def plot_signal(signal, start, end, x_axis, name):
+    # This is a weighting value to move the scale of the signal up from the 0-line. This is to avoid errors with signs
+    m = 200
+
+    # Vectors to store the positive and negative values of the signal are initialized
+    vec_pos = []
+    vec_neg = []
+
+    # Iterating through the values in the signal and adding them to the correct vector. 0 is added if the value is not added to preserve the shape of the vectors in the plotting
+    for elem in signal:
+        if elem < 0:
+            # 0 is added if the value is not the corresponding sign, to preserve the shape of the vectors in the plotting
+            vec_pos.append(0)
+            vec_neg.append(elem)
+        if elem >= 0:
+            vec_pos.append(elem)
+            vec_neg.append(0)
+    # The signal is elevated by m, such that all peaks are included, even those that might register below zero
+    pos_weighted_signal = [value+m for value in signal]
+    # The signal is flipped by multiplying with -1, and elevated by m. This is to turn all valleys in the signal to peaks
+    neg_weighted_signal = [(value*(-1))+m for value in signal]
+
+    # The indices of all the peaks and valleys are calculated
+    pos_peaks = sp.signal.find_peaks(
+        pos_weighted_signal, height=m*0.25, distance=30)[0]
+    neg_peaks = sp.signal.find_peaks(
+        neg_weighted_signal, height=m*0.25, distance=30)[0]
+
+    # The vectors for the peaks and valleys are initialized. The value None is used in this part, since it does not show in plotting
+    pos_amplitude = [None]
+    neg_amplitude = [None]
+
+    # Variable to keep track of the last selected peak index
+    last_peak = 0
+
+    # Iterating through the positive peaks
+    for peak in pos_peaks:
+        # Between the peaks, the value None is added where all other signal data was before. This way we only preserve the amplitudes of the peaks in the signal
+        pos_amplitude.extend([None]*(peak-last_peak-1))
+
+        # We add the amplitude to the vector
+        if vec_pos[peak] > 0:
+            pos_amplitude.append(vec_pos[peak])
+
+        # If the value returns zero from the positive part of the signal, then we have a peak in the negative part of the signal
+        else:
+            pos_amplitude.append(vec_neg[peak])
+
+        # The selected peak is updated to the last peak and the iteration is complete
+        last_peak = peak
+
+    # The remaining values of None is added to the end of the positive amplitude vector
+    pos_amplitude.extend([None]*((end - start) - len(pos_amplitude)))
+
+    # --- This is the same procedure as with the positive amplitudes. Only this time it is the negative amplitudes
+    last_peak = 0
+    for peak in neg_peaks:
+        neg_amplitude.extend([None]*(peak-last_peak-1))
+        if vec_neg[peak] < 0:
+            neg_amplitude.append(vec_neg[peak])
+        else:
+            neg_amplitude.append(vec_pos[peak])
+        last_peak = peak
+    neg_amplitude.extend([None]*((end - start) - len(neg_amplitude)))
+    # ---
+
+    # \-- Plotting --/
+
+    # The amplitudes are plotted as points
+    plt.plot(x_axis, pos_amplitude, 'ro')
+    plt.plot(x_axis, neg_amplitude, 'bo')
+
+    # The positive and negative part of the signal is plotted
+    plt.plot(x_axis, vec_pos, color='sandybrown')
+    plt.plot(x_axis, vec_neg, color='lightskyblue')
+
+    # A line is plotted where the maximum value is
+    plt.hlines(max(signal), start, end, color='lime')
+    plt.hlines(min(signal), start, end, color='lime')
+
+    # Title is selected
+    plt.title(name + ' | Peaks = ' + str(len(pos_peaks) + len(neg_peaks)) + ' | Max value = ' + str(np.round(max(signal), 3)) +
+              ', Min value = ' + str(np.round(min(signal), 3)))
+    plt.show()
